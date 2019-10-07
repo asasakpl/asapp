abbbced3b3066951ee2b264623a4284263bb914e
SerekKiri <kacper.potyrala@gmail.com>
2019-10-07 19:03:09 +0200

    feat: First project build

---
+++ src/main/index.js
@@ -0,0 +1,65 @@
+import { app, BrowserWindow } from 'electron'
+
+/**
+ * Set `__static` path to static files in production
+ * https://simulatedgreg.gitbooks.io/electron-vue/content/en/using-static-assets.html
+ */
+if (process.env.NODE_ENV !== 'development') {
+  global.__static = require('path')
+    .join(__dirname, '/static')
+    .replace(/\\/g, '\\\\')
+}
+
+let win
+const winURL =
+  process.env.NODE_ENV === 'development'
+    ? `http://localhost:9080`
+    : `file://${__dirname}/index.html`
+
+function createWindow() {
+  /**
+   * Initial window options
+   */
+  win = new BrowserWindow({
+    useContentSize: true,
+
+    fullscreen: true,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  })
+
+  win.removeMenu()
+
+  win.loadURL(winURL)
+
+  win.openDevTools()
+
+  win.on('closed', () => {
+    win = null
+  })
+}
+
+app.on('ready', createWindow)
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  if (win === null) {
+    createWindow()
+  }
+})
+
+import { autoUpdater } from 'electron-updater'
+
+autoUpdater.on('update-downloaded', () => {
+  autoUpdater.quitAndInstall()
+})
+
+app.on('ready', () => {
+  if (process.env.NODE_ENV === 'production') autoUpdater.checkForUpdates()
+})

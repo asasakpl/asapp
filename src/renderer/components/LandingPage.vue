a23cf74af79e5c10617ff49c0c3862cc8e0f8b24
SerekKiri <kacper.potyrala@gmail.com>
2019-10-07 19:26:42 +0200

    style: Cleaned project

--- src/renderer/components/LandingPage.vue
+++ src/renderer/components/LandingPage.vue
@@ -7,16 +7,9 @@
 </template>
 
 <script>
-import SystemInformation from "./LandingPage/SystemInformation";
-
 export default {
   name: "landing-page",
-  components: { SystemInformation },
-  methods: {
-    open(link) {
-      this.$electron.shell.openExternal(link);
-    }
-  }
+  methods: {}
 };
 </script>
 

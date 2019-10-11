dca6e38338d3d3076f1ec1e72007aa5a0c686cfa
SerekKiri <kacper.potyrala@gmail.com>
2019-10-11 22:42:42 +0200

    fix: Login page render

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,14 +1,11 @@
 <template>
   <v-app>
     <v-content>
-      <v-container class="fill-height" height="100%" fluid>
+      <v-container class="fill-height" fluid>
         <v-layout text-center wrap>
           <v-flex xs12>
-            <v-img :src="require('../assets/logo.png')" class="my-3 mt-5 mb-5" contain height="200"></v-img>
-          </v-flex>
-
-          <v-flex mb-5 xs12>
-            <h2 class="headline font-weight-bold mb-3 mt-6">Zaloguj się</h2>
+            <v-img :src="require('../assets/logo.png')" class="mt-5" contain height="200"></v-img>
+            <h2 class="headline font-weight-bold mb-3">Zaloguj się</h2>
 
             <v-layout justify-center>
               <v-form @submit.prevent="login">

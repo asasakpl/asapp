436b552c8d30b9b51f149d0c87d47bc4ced6c1ee
SerekKiri <kacper.potyrala@gmail.com>
2019-10-21 18:20:04 +0200

    feat: New login background

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,58 +1,74 @@
 <template>
-  <v-app>
-    <v-content>
-      <v-container class="fill-height" fluid>
-        <v-layout text-center wrap>
-          <v-flex xs12>
-            <v-img :src="require('../assets/logo.png')" class="mt-5" contain height="200"></v-img>
-            <h2 class="headline font-weight-bold mb-3">Zaloguj się</h2>
+  <div class="back">
+    <v-app id="no-back">
+      <v-content>
+        <v-container class="fill-height" fluid>
+          <v-layout text-center wrap>
+            <v-flex xs12>
+              <div class="space"></div>
 
-            <v-layout justify-center>
-              <v-form @submit.prevent="login">
-                <v-row style="width: 330px;">
-                  <v-col cols="12">
-                    <v-text-field
-                      outlined
-                      v-model="email"
-                      label="Email"
-                      name="email"
-                      prepend-inner-icon="mail"
-                      type="text"
-                      color="green accent-4"
-                    ></v-text-field>
+              <v-layout justify-center>
+                <v-form @submit.prevent="login">
+                  <v-row style="width: 330px;">
+                    <v-col cols="12">
+                      <v-text-field
+                        dark
+                        outlined
+                        v-model="email"
+                        label="Email"
+                        name="email"
+                        prepend-inner-icon="mail"
+                        type="text"
+                      ></v-text-field>
 
-                    <v-text-field
-                      v-model="password"
-                      outlined
-                      id="password"
-                      label="Password"
-                      name="password"
-                      prepend-inner-icon="lock"
-                      type="password"
-                      color="green accent-4"
-                    ></v-text-field>
+                      <v-text-field
+                        dark
+                        v-model="password"
+                        outlined
+                        id="password"
+                        label="Password"
+                        name="password"
+                        prepend-inner-icon="lock"
+                        type="password"
+                      ></v-text-field>
 
-                    <v-btn color="green accent-4" class="white--text" type="sumbit">Zaloguj</v-btn>
-                  </v-col>
-                </v-row>
-              </v-form>
-            </v-layout>
-          </v-flex>
-        </v-layout>
-      </v-container>
-    </v-content>
-    <v-footer justify-center class="grey--text">
-      <div class="flex-grow-1"></div>
-      <div>asapp version {{ $store.getters.appVersion }}</div>
-    </v-footer>
-  </v-app>
+                      <v-btn type="sumbit">Zaloguj</v-btn>
+                    </v-col>
+                  </v-row>
+                </v-form>
+              </v-layout>
+            </v-flex>
+          </v-layout>
+        </v-container>
+      </v-content>
+
+      <v-footer justify-center class="grey--text" id="no-back">
+        <div class="flex-grow-1"></div>
+        <div>asapp version {{ $store.getters.appVersion }}</div>
+      </v-footer>
+    </v-app>
+  </div>
 </template>
+
+<style scoped>
+.back {
+  background-image: url(../assets/login_background.png);
+  background-size: cover;
+}
+#no-back {
+  background: none;
+}
+.space {
+  height: 400px;
+}
+</style>
 
 <script lang="ts">
 import Vue from "vue";
 
 export default Vue.extend({
   data() {
+    image: "../assets/login_background";
     return {
       email: "",
       password: ""

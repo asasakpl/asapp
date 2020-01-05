a9f216f5f8ca859b948f8c2119403d28e33c99b1
SerekKiri <kacper.potyrala@gmail.com>
2020-01-05 18:21:50 +0100

    feat: Dashboard layout

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,25 +1,81 @@
 <template>
-  <div>
-    <Default />
+  <v-container>
+    <v-row justify="space-around">
+      <v-navigation-drawer
+        v-model="drawer"
+        :color="color"
+        :expand-on-hover="expandOnHover"
+        :mini-variant="miniVariant"
+        :right="right"
+        :src="bg"
+        absolute
+        dark
+      >
+        <v-list dense nav class="py-0">
+          <v-list-item two-line>
+            <v-avatar size="42">
+              <v-icon large>mdi-account</v-icon>
+            </v-avatar>
 
-    <span>{{ user }}</span>
-  </div>
+            <v-list-item-content>
+              <v-list-item-title>{{ user.firstName }}</v-list-item-title>
+              <v-list-item-subtitle>{{ user.lastName }}</v-list-item-subtitle>
+            </v-list-item-content>
+          </v-list-item>
+
+          <v-divider></v-divider>
+
+          <v-list-item v-for="item in items" :key="item.title" link>
+            <v-list-item-icon>
+              <v-icon>{{ item.icon }}</v-icon>
+            </v-list-item-icon>
+
+            <v-list-item-content>
+              <v-list-item-title>{{ item.title }}</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list>
+      </v-navigation-drawer>
+
+      <span>{{ user }} </span>
+
+      <v-footer app>
+        <v-spacer />
+        <span>&copy; 2019</span>
+      </v-footer>
+    </v-row>
+  </v-container>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
 import axios from 'axios'
-import Default from '../layouts/Default.vue'
 
 export default Vue.extend({
+  props: {
+    source: String
+  },
+  created() {
+    this.$vuetify.theme.dark = true
+  },
   data() {
     return {
-      user: null
+      user: null,
+      drawer: true,
+      items: [
+        { title: 'Dashboard', icon: 'mdi-view-dashboard' },
+        { title: 'Orders', icon: 'mdi-truck' },
+        { title: 'About', icon: 'mdi-help-box' }
+      ],
+      color: 'red',
+      colors: ['primary', 'blue', 'success', 'red', 'teal'],
+      right: false,
+      miniVariant: false,
+      expandOnHover: true,
+      background: false
     }
   },
-  components: {
-    Default
-  },
+  components: {},
   methods: {
     async getUserdata() {
       let id = localStorage.getItem('m_user')

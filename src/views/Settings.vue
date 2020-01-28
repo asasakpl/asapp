93c73a50b12843049c38f81ed84354694bc65af8
SerekKiri <kacper.potyrala@gmail.com>
2020-01-28 19:26:12 +0100

    feat: Changing theme in settings

--- src/views/Settings.vue
+++ src/views/Settings.vue
@@ -1,13 +1,40 @@
 <template>
-  <v-container></v-container>
+  <v-container>
+    <v-card class="mx-auto px-auto round" tile round max-width="90%">
+      <v-card-title>
+        Ustawienia
+      </v-card-title>
+      <v-card-title>
+        <v-switch
+          color="primary"
+          label="Motyw aplikacji"
+          v-model="themeColor"
+          @change="changeTheme()"
+        ></v-switch>
+      </v-card-title>
+    </v-card>
+  </v-container>
 </template>
+
+<style scoped>
+.round {
+  border-radius: 20px;
+}
+</style>
 
 <script lang="ts">
 import Vue from 'vue'
 
 export default Vue.extend({
   data() {
-    return {}
+    return {
+      themeColor: this.$vuetify.theme.dark
+    }
+  },
+  methods: {
+    changeTheme() {
+      this.$vuetify.theme.dark = !this.$vuetify.theme.dark
+    }
   }
 })
 </script>

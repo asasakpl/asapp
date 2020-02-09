e3bc45921441190afbd959717af0e42212c867a0
SerekKiri <kacper.potyrala@gmail.com>
2020-02-09 14:16:31 +0100

    feat: Now product list display pictures of products

---
+++ src/components/Products/ProductsList.vue
@@ -0,0 +1,103 @@
+<template>
+  <v-container>
+    <v-card class="mx-auto px-auto" max-width="90%" tile>
+      <v-card-title>
+        Lista produktów
+        <v-spacer></v-spacer>
+        <v-text-field
+          v-model="search"
+          append-icon="search"
+          label="Search"
+          color="white"
+          single-line
+          hide-details
+        ></v-text-field>
+      </v-card-title>
+      <v-data-table
+        v-if="!error"
+        v-model="selected"
+        :headers="headers"
+        :items="products"
+        sort-by="id"
+        item-key="id"
+        :search="search"
+        class="elevation-1"
+        :page.sync="page"
+        hide-default-footer
+        :items-per-page="16"
+        @page-count="pageCount = $event"
+      >
+        <template v-slot:item.image="{ item }">
+          <div class="mx-2 mt-1 mb-1">
+            <v-img
+              :src="item.pictures[0].url"
+              :alt="item.id"
+              height="80px"
+              width="80px"
+            ></v-img>
+          </div>
+        </template>
+      </v-data-table>
+      <v-data-table
+        v-else
+        loading
+        hide-default-footer
+        loading-text="Loading... Please wait"
+      ></v-data-table>
+    </v-card>
+    <div class="text-center pt-2">
+      <v-pagination v-model="page" :length="pageCount"></v-pagination>
+    </div>
+  </v-container>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import axios from 'axios'
+
+export default Vue.extend({
+  data() {
+    return {
+      error: true,
+      page: 1,
+      pageCount: 5,
+      products: [],
+      search: '',
+      singleSelect: true,
+      selected: [],
+      headers: [
+        {
+          text: 'id',
+          align: 'left',
+          sortable: true,
+          value: 'id'
+        },
+        { text: 'Picture', value: 'image', sortable: false },
+        { text: 'Nazwa', value: 'title.pl' },
+        { text: 'Typ', value: 'type' },
+        { text: 'Tag', value: 'tag' },
+        { text: 'Kategoria', value: 'category' }
+      ]
+    }
+  },
+  methods: {
+    getProducts() {
+      let id = localStorage.getItem('m_user')
+      axios
+        .get(`http://localhost:3000/v1/products`)
+        .then(res => {
+          this.error = false
+          localStorage.setItem('token', res.config.headers.auth)
+          this.products = res.data.data.products
+          return
+        })
+        .catch(err => {
+          this.error = true
+        })
+    }
+  },
+  created() {
+    this.getProducts()
+  }
+})
+</script>

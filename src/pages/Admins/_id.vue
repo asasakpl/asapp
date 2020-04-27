4a3d3b6ccdf6c900d319096af9c1297236d8e950
SerekKiri <kacper.potyrala@gmail.com>
2020-04-27 13:01:47 +0200

    remove: Option to edit admin (endpoint not ready yet)

--- src/pages/Admins/_id.vue
+++ src/pages/Admins/_id.vue
@@ -1,95 +1,71 @@
 <template>
   <v-container>
-    <v-row>
-      <v-btn @click="$router.go(-1)" icon class="ml-8">
-        <v-icon size="32">arrow_back</v-icon>
-      </v-btn>
-      <v-content v-if="admin" style="overflow: scroll">
-        <v-card
-          class="mx-auto px-auto pb-2 round"
-          style="overflow: scroll;"
-          max-width="90%"
-          max-height="80vh"
-          tile
-        >
-          <v-card-title class="pb-2">
-            {{ $t('admin.page.id') }}: {{ admin.id }}
-            <v-spacer></v-spacer>
-            <v-btn @click="editAdmin" large icon class="mb-1 pb-1">
-              <v-icon size="32" class="">mdi-pencil</v-icon>
-            </v-btn>
-          </v-card-title>
-          <v-divider></v-divider>
+    <v-row v-if="admin">
+      <v-col cols="1" align="end">
+        <v-btn @click="$router.push('/admins')" icon fab class="mt-2">
+          <v-icon size="38">arrow_back</v-icon>
+        </v-btn>
+      </v-col>
+      <v-col>
+        <v-content class="pa-0 pl-5" style="overflow: scroll">
+          <v-card class="round" style="overflow: scroll;" max-width="90%" max-height="80vh" tile>
+            <v-card-title class="pb-2">
+              {{ $t('admin.page.id') }}: {{ admin.id }}
+              <v-spacer></v-spacer>
+              <v-btn @click="editAdmin" large icon class="mb-1 pb-1">
+                <v-icon size="32" class>mdi-pencil</v-icon>
+              </v-btn>
+            </v-card-title>
+            <v-divider></v-divider>
 
-          <v-row>
-            <v-col cols="4" class="pr-2 ml-6">
-              <v-col class="pb-0">
-                <div class="headline ml-1 mb-3">
-                  {{ $t('admin.page.name') }}
-                </div>
-                <v-text-field
-                  v-model="admin.firstName"
-                  outlined
-                  :disabled="disabled"
-                ></v-text-field>
+            <v-row>
+              <v-col cols="4" class="pr-2 ml-6">
+                <v-col class="pb-0">
+                  <div class="headline ml-1 mb-3">{{ $t('admin.page.name') }}</div>
+                  <v-text-field v-model="admin.firstName" outlined :disabled="disabled"></v-text-field>
+                </v-col>
+                <v-col class="pb-0">
+                  <div class="headline ml-1 mb-3">{{ $t('admin.page.email') }}</div>
+                  <v-text-field
+                    v-model="admin.email"
+                    outlined
+                    :disabled="disabled"
+                    :rules="emailRules"
+                  ></v-text-field>
+                </v-col>
               </v-col>
-              <v-col class="pb-0">
-                <div class="headline ml-1 mb-3">
-                  {{ $t('admin.page.email') }}
-                </div>
-                <v-text-field
-                  v-model="admin.email"
-                  outlined
-                  :disabled="disabled"
-                  :rules="emailRules"
-                ></v-text-field>
+              <v-col cols="4" class="pr-2 ml-6">
+                <v-col class="pb-0">
+                  <div class="headline ml-1 mb-3">{{ $t('admin.page.surname') }}</div>
+                  <v-text-field v-model="admin.lastName" outlined :disabled="disabled"></v-text-field>
+                </v-col>
+                <v-col class="pb-0">
+                  <div class="headline ml-1 mb-3">{{ $t('admin.page.type') }}</div>
+                  <v-select
+                    :items="types"
+                    :disabled="disabled"
+                    v-model="admin.type"
+                    item-text="title"
+                    v-bind:label="$t('order.delivery.products.dialog.set_status')"
+                  ></v-select>
+                </v-col>
               </v-col>
-            </v-col>
-            <v-col cols="4" class="pr-2 ml-6">
-              <v-col class="pb-0">
-                <div class="headline ml-1 mb-3">
-                  {{ $t('admin.page.surname') }}
-                </div>
-                <v-text-field
-                  v-model="admin.lastName"
-                  outlined
-                  :disabled="disabled"
-                ></v-text-field>
-              </v-col>
-              <v-col class="pb-0">
-                <div class="headline ml-1 mb-3">
-                  {{ $t('admin.page.type') }}
-                </div>
-                <v-select
-                  :items="types"
-                  :disabled="disabled"
-                  v-model="admin.type"
-                  item-text="title"
-                  v-bind:label="$t('order.delivery.products.dialog.set_status')"
-                >
-                </v-select>
-              </v-col>
-            </v-col>
-          </v-row>
-
-          <v-row class="mr-3">
-            <v-spacer></v-spacer>
-            <v-btn @click="cancelEdit()" class="mr-3" v-show="save"
-              >Cancel</v-btn
-            >
-
-            <v-btn @click="saveAdmin(admin)" color="primary" v-show="save"
-              >Save</v-btn
-            >
-          </v-row>
-        </v-card>
-      </v-content>
-      <v-content v-else align="center">
-        <v-progress-circular
-          indeterminate
-          color="primary"
-        ></v-progress-circular>
-      </v-content>
+            </v-row>
+          </v-card>
+        </v-content>
+      </v-col>
+    </v-row>
+    <v-row v-else>
+      <v-col>
+        <v-btn @click="$router.push('/admins')" icon fab class="mt-2">
+          <v-icon size="38">arrow_back</v-icon>
+        </v-btn>
+      </v-col>
+      <v-col cols="7" class="mr-12">
+        <v-content>
+          <v-progress-circular indeterminate color="primary"></v-progress-circular>
+        </v-content>
+      </v-col>
     </v-row>
 
     <NetworkError :error="error"></NetworkError>
@@ -103,13 +79,13 @@
 </style>
 
 <script lang="ts">
-import Vue from 'vue'
-import axios from 'axios'
-import NetworkError from '@/components/NetworkError.vue'
+import Vue from "vue";
+import axios from "axios";
+import NetworkError from "@/components/NetworkError.vue";
 
 export default Vue.extend({
   components: {
-    NetworkError,
+    NetworkError
   },
   data() {
     return {
@@ -119,37 +95,37 @@ export default Vue.extend({
       error: true,
       types: [0, 1],
       emailRules: [
-        (v) =>
+        v =>
           !v ||
           /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(v) ||
-          'E-mail must be valid',
-      ],
-    }
+          "E-mail must be valid"
+      ]
+    };
   },
   methods: {
     editAdmin() {
-      this.disabled = false
-      this.save = true
+      this.disabled = false;
+      this.save = true;
     },
     saveAdmin(admin) {
-      console.log(admin)
+      console.log(admin);
     },
     cancelEdit() {
-      this.disabled = true
-      this.save = false
-    },
+      this.disabled = true;
+      this.save = false;
+    }
   },
   async mounted() {
     await axios
       .get(`/admins/${this.$route.params.id}`)
-      .then((res) => {
-        this.error = false
-        this.admin = res.data.data.admin
+      .then(res => {
+        this.error = false;
+        this.admin = res.data.data.admin;
       })
-      .catch((err) => {
-        this.error = true
-        console.log(err)
-      })
-  },
-})
+      .catch(err => {
+        this.error = true;
+        console.log(err);
+      });
+  }
+});
 </script>

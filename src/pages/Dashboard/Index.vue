12702ca05f2effa5f154851ab98338879e05eca7
SerekKiri <kacper.potyrala@gmail.com>
2020-05-21 12:55:28 +0200

    fix: Dashboard grid on lower res

--- src/pages/Dashboard/Index.vue
+++ src/pages/Dashboard/Index.vue
@@ -1,10 +1,10 @@
 <template>
   <v-container>
-    <v-card class="mx-auto px-auto pb-5 round grid" tile>
+    <v-card class="mx-auto px-auto pb-5 round" max-width="90%" tile>
       <v-card-title>{{ $t('dashboard.statistics.title') }}</v-card-title>
       <v-col cols="12">
         <v-row>
-          <v-col class="grid">
+          <v-col>
             <v-card class="mx-auto pr-4" color="primary" dark min-width="250">
               <div class="d-flex flex-no-wrap justify-space-between">
                 <v-card-text>
@@ -16,35 +16,32 @@
               </div>
             </v-card>
           </v-col>
-          <v-col class="grid">
+          <v-col>
             <v-card class="mx-auto pr-4" color="primary" dark min-width="250">
-              <div>
+              <div class="d-flex flex-no-wrap justify-space-between">
                 <v-card-text>
-                  <div>
-                    <div>{{ $t('dashboard.statistics.orders_card') }}</div>
-                  </div>
-                  <div class="d-flex flex-no-wrap justify-space-between">
-                    <p class="display-1 ma-0 pt-1">{{ this.orders }}</p>
-                    <v-icon size="50">mdi-truck</v-icon>
-                  </div>
+                  <div>{{ $t('dashboard.statistics.orders_card') }}</div>
+
+                  <p class="display-1">{{ this.orders }}</p>
                 </v-card-text>
+
+                <v-icon size="50">mdi-truck</v-icon>
               </div>
             </v-card>
           </v-col>
-          <v-col class="grid">
+          <v-col>
             <v-card class="mx-auto pr-4" color="primary" dark min-width="250">
-              <div>
+              <div class="d-flex flex-no-wrap justify-space-between">
                 <v-card-text>
-                  <div>{{ $t('dashboard.statistics.products_card') }}</div>
-                  <div class="d-flex flex-no-wrap justify-space-between">
-                    <p class="display-1 ma-0 pt-1">{{ this.products }}</p>
-                    <v-icon size="50">mdi-folder-home</v-icon>
-                  </div>
+                  {{ $t('dashboard.statistics.products_card') }}
+                  <p class="display-1">{{ this.products }}</p>
                 </v-card-text>
+
+                <v-icon size="50">mdi-folder-home</v-icon>
               </div>
             </v-card>
           </v-col>
-          <v-col class="grid">
+          <v-col>
             <v-card class="mx-auto pr-4" color="primary" dark min-width="250">
               <div class="d-flex flex-no-wrap justify-space-between">
                 <v-card-text>
@@ -74,10 +71,10 @@
 </style>
 
 <script lang="ts">
-import Vue from "vue";
-import axios from "axios";
-import DashboardLayout from "@/layouts/DashboardLayout.vue";
-import NetworkError from "@/components/NetworkError.vue";
+import Vue from "vue"
+import axios from "axios"
+import DashboardLayout from "@/layouts/DashboardLayout.vue"
+import NetworkError from "@/components/NetworkError.vue"
 
 export default Vue.extend({
   components: {
@@ -92,58 +89,58 @@ export default Vue.extend({
       orders: 0,
       users: 0,
       name: localStorage.getItem("m_name")
-    };
+    }
   },
   methods: {
     async getData() {
-      let date = new Date();
-      date.setDate(date.getDate() - 30);
+      let date = new Date()
+      date.setDate(date.getDate() - 30)
 
       await axios
         .get("/users")
         .then(res => {
-          this.error = false;
-          let users = res.data.data.users;
-          users = users.map(user => user.createdAt);
+          this.error = false
+          let users = res.data.data.users
+          users = users.map(user => user.createdAt)
 
           for (let x in users) {
-            let usersDate = new Date(users[x]);
-            let ddd = 0;
+            let usersDate = new Date(users[x])
+            let ddd = 0
             if (date < usersDate) {
-              ddd++;
+              ddd++
             }
-            this.users = this.users + ddd;
+            this.users = this.users + ddd
           }
         })
         .catch(err => {
-          this.error = true;
-        });
+          this.error = true
+        })
 
       await axios.get("/products").then(res => {
-        this.products = res.data.data.products.length;
-      });
+        this.products = res.data.data.products.length
+      })
 
       await axios.get("/orders").then(res => {
-        let orders = res.data.data.orders;
-        orders = orders.map(order => order.createdAt);
+        let orders = res.data.data.orders
+        orders = orders.map(order => order.createdAt)
 
         for (let x in orders) {
-          let orderDate = new Date(orders[x]);
-          let ddd = 0;
+          let orderDate = new Date(orders[x])
+          let ddd = 0
           if (date < orderDate) {
-            ddd++;
+            ddd++
           }
-          this.orders = this.orders + ddd;
+          this.orders = this.orders + ddd
         }
-      });
+      })
 
       await axios.get("/owners").then(res => {
-        this.sellers = res.data.data.owners.length;
-      });
+        this.sellers = res.data.data.owners.length
+      })
     }
   },
   beforeMount() {
-    this.getData();
+    this.getData()
   }
-});
+})
 </script>
